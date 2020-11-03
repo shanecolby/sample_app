12b926f82c2799d48b48ad0eab473670cf84c014
shane <shanecolbyzero8@gmail.com>
2020-11-02 18:36:14 -0600

    added time to message

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,6 +1,6 @@
 class Api::ExamplePagesController < ApplicationController
   def hello_action
-    render json: {message: "Hello this is a test"}
+    render json: {message: "Hello this is a test", time: Time.now}
   end
 end
 
